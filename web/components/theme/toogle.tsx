2fae10d432b43cd0dfa9d301e276875b177e3dea
rinsvent <rinsvent007@gmail.com>
2025-06-01 22:28:31 +0700

    feat: fix styles

--- web/components/theme/toogle.tsx
+++ web/components/theme/toogle.tsx
@@ -1,19 +1,20 @@
 'use client'
 
-import { useTheme } from '@/context/ThemeContext'
+import {useTheme} from '@/context/ThemeContext'
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import IconButton from "@mui/material/IconButton";
 
 export function ThemeToggle() {
-    const { theme, toggleTheme } = useTheme()
+    const {theme, toggleTheme} = useTheme()
 
     return (
-        <button
+        <IconButton
             onClick={toggleTheme}
             className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700"
             aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
         >
-            {theme === 'light' ? <LightModeIcon /> : <DarkModeIcon />}
-        </button>
+            {theme === 'light' ? <LightModeIcon/> : <DarkModeIcon/>}
+        </IconButton>
     )
 }
