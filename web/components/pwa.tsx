74e9e7fcf3da62bc6aba0fb87b3d7fb58db65a45
Sipachev Igor <sipachev@sveak.com>
2025-06-02 23:03:52 +0700

    feat: add permission request for notification

--- web/components/pwa.tsx
+++ web/components/pwa.tsx
@@ -3,10 +3,23 @@
 import { useEffect } from 'react'
 import { Workbox } from 'workbox-window'
 
+async function requestNotificationPermission() {
+    const permission = await Notification.requestPermission();
+    if (permission !== 'granted') {
+        console.error('Разрешение не получено!');
+        return false;
+    }
+    return true;
+}
+
 export function PWA() {
     useEffect(() => {
         const wb = new Workbox("sw.js", {scope: "/"})
         wb.register()
+            .then(() => {
+                return requestNotificationPermission()
+            })
+
     }, [])
     return <></>
 }
