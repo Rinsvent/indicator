2ee81e5bc53f26122f504c971060b95864eac97d
Sipachev Igor <sipachev@sveak.com>
2025-07-15 21:06:21 +0700

    feat: add support background task

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"git.rinsvent.ru/rinsvent/indicator/internal/controller"
+	"git.rinsvent.ru/rinsvent/indicator/internal/service"
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 	"github.com/urfave/cli/v3"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -23,6 +27,14 @@ func main() {
 				Aliases:  []string{"s"},
 				Usage:    "Start web server",
 				Action: func(ctx context.Context, c *cli.Command) error {
+					cr := cron.New(cron.WithSeconds(), cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)))
+					cr.AddFunc("*/5 * * * * *", func() {
+						fmt.Println("Background check was handled")
+						service.IM().Check()
+						time.Sleep(time.Second * 20)
+					})
+					cr.Start()
+
 					err := http.ListenAndServe("0.0.0.0:8012", controller.MakeRouter())
 					if err != nil {
 						return err
